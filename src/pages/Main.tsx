34979fe417aae9cf8f3c0d6095d61e7521deec1e
dircosine <zidhdy@gmail.com>
2020-04-10 18:24:51 +0900

    Setup ant-design

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function Main() {
-  return <p>'test'</p>;
+  return (
+    <div className="Main">
+      <Button type="primary">Button</Button>
+    </div>
+  );
 }
 
 export default Main;
