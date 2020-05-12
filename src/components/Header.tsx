545df4fb32a0ae271943c9a21530d18b6e00169b
dircosine <zidhdy@gmail.com>
2020-05-12 20:12:53 +0900

    Prevent Link to forum when no tournament record

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Menu, Modal } from 'antd';
+import { Layout, Menu, Modal, message } from 'antd';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { ClickParam } from 'antd/lib/menu';
 import { Logo } from '../img';
 import Emoji from './Emoji';
+import { getTargetEventDate } from '../lib/utils';
 
 interface HeaderProps extends RouteComponentProps {}
 
 function Header({ location, history }: HeaderProps) {
   const [current, setCurrent] = useState(location.pathname);
-  const handleMenuClick = (e: ClickParam) => {
-    if (e.key === '/scorebook') return;
-    setCurrent(e.key);
-  };
 
   useEffect(() => {
     setCurrent(location.pathname);
   }, [location]);
 
-  const prepareInfoModal = () => {
-    Modal.info({
-      title: '채점 기능은 지금 열심히 만들고 있습니다...',
-      content: (
-        <div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <p>오늘 직접 선정한 토너먼트 결과를 가지고,</p>
-          <p>
-            <strong>향후의 실제 주가 변동에 따른 수익률을 계산해 드립니다! </strong>
-          </p>
-          <br />
-          <p>
-            내 차트보는 눈이 어느 정도인지 확인해 보세요 <Emoji symbol="😆" />
-          </p>
-        </div>
-      ),
-      onOk() {},
-    });
+  const handleMenuClick = (e: ClickParam) => {
+    if (e.key === '/scorebook') {
+      Modal.info({
+        title: '채점 기능은 지금 열심히 만들고 있습니다...',
+        content: (
+          <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>오늘 직접 선정한 토너먼트 결과를 가지고,</p>
+            <p>
+              <strong>향후의 실제 주가 변동에 따른 수익률을 계산해 드립니다! </strong>
+            </p>
+            <br />
+            <p>
+              내 차트보는 눈이 어느 정도인지 확인해 보세요 <Emoji symbol="😆" />
+            </p>
+          </div>
+        ),
+        onOk() {},
+      });
+    } else if (e.key === '/forum') {
+      if (hasRecord()) {
+        history.push('/forum');
+      } else {
+        message.warning('먼저 오늘의 토너먼트를 완료해 주세요', 100);
+      }
+    } else {
+      setCurrent(e.key);
+    }
+  };
+
+  const hasRecord = (): boolean => {
+    const targetEventDate = getTargetEventDate(new Date());
+    const doneDates: string[] = JSON.parse(localStorage.getItem('doneDates') || '[]');
+    return doneDates.includes(targetEventDate) ? true : false;
   };
 
   return (
@@ -55,12 +68,8 @@ function Header({ location, history }: HeaderProps) {
         <Menu.Item key="/">
           {current !== '/' ? <Link to="/">오늘의 토너먼트</Link> : '오늘의 토너먼트'}
         </Menu.Item>
-        <Menu.Item key="/forum">
-          <Link to="/forum">객장</Link>
-        </Menu.Item>
-        <Menu.Item key="/scorebook" onClick={prepareInfoModal}>
-          채점
-        </Menu.Item>
+        <Menu.Item key="/forum">객장</Menu.Item>
+        <Menu.Item key="/scorebook">채점</Menu.Item>
       </Menu>
     </Layout.Header>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,22 @@ export const shuffle = <T>(a: T[]): T[] => {
 };
 
 export const getYYYYMMDD = (d: Date): string => {
-  const month =
-    d.getMonth() < 9 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
+  const month = d.getMonth() < 9 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
   const date = d.getDate() < 10 ? `0${d.getDate()}` : `${d.getDate()}`;
 
   return `${d.getFullYear()}-${month}-${date}`;
+};
+
+export const getTargetEventDate = (now: Date): string => {
+  if (now.getUTCHours() < 10) {
+    now.setDate(now.getDate() - 1);
+  }
+  return formatEventDate(now);
+};
+
+export const formatEventDate = (d: Date): string => {
+  const month = d.getMonth() < 9 ? `0${d.getMonth() + 1}` : `${d.getMonth() + 1}`;
+  const date = d.getUTCDate() < 10 ? `0${d.getUTCDate()}` : `${d.getUTCDate()}`;
+
+  return `${d.getFullYear()}${month}${date}`;
 };
